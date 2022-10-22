06d46a3c08e844a73fe3dda2ad6d26d25b844890
hetyey-b <hetyey.botond@gmail.com>
2022-10-22 10:35:09 +0200

    frontend - burger menu icons + styling + animation

--- client/src/components/BurgerMenu.js
+++ client/src/components/BurgerMenu.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import {ImCross, ImMenu} from 'react-icons/im'
 
-const BurgerMenu = ({content}) => {
-    const [open,setOpen] = React.useState(false);
-
+const BurgerMenu = ({content, open, setOpen}) => {
     const handleOnClick = () => {
-        console.log('click!');
         setOpen(!open); 
     }
 
@@ -13,22 +10,38 @@ const BurgerMenu = ({content}) => {
         <div className='absolute'>
             {
                 open ?
-                <ImCross 
-                    className='text-bgGray w-[20px] h-[20px] m-3 cursor-pointer'
-                    onClick={handleOnClick}
-                />
-                :
-                <ImMenu 
-                    className='text-bgGray w-[25px] h-[25px] m-2 cursor-pointer'
-                    onClick={handleOnClick}
-                />
+                    <ImCross 
+                        className='text-bgGray w-[20px] h-[20px] m-3 cursor-pointer'
+                        onClick={handleOnClick}
+                    />
+                    :
+                    <ImMenu 
+                        className='text-bgGray w-[25px] h-[25px] m-2 cursor-pointer'
+                        onClick={handleOnClick}
+                    />
             }
             <div
-                className={`bg-brandBlue text-bgGray w-[30vw] py-2 px-4 mx-1 
+                className={`bg-brandBlue text-bgGray w-[30vw] py-2 px-4 h-[93vh] shadow-inner shadow-lg rounded
                             ${open ? 'translate-x-0 visible opacity-100' : 'translate-x-[-100%] invisible opacity-0'}
                             ease-in-out duration-300`}
             >
-                {content}
+                <ul>
+                    {content.map((menuOption, ind) => 
+                        <li
+                            onClick={menuOption.onClick}
+                            className={`w-full cursor-pointer hover:bg-brandBlueHover flex justify-between
+                                        px-2 py-4 border-b-2 ${ind === 0 && 'border-t-2 mt-2'} border-bgGray font-bold`}
+                        >
+                            <div className='inline-block'>
+                                {menuOption.icon}
+                            </div>
+                            <div className='inline-block uppercase text-xl'>
+                                {menuOption.text}
+                            </div>
+                        </li>
+                    )
+                    }
+                </ul>
             </div>
         </div>
     )
